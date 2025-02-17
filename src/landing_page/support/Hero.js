f28c4cf7e82e0f3f86c03711799c909cd0a02018
Disha Rathore <disharathore126@gmail.com>
2025-02-17 14:31:35 +0530

    New commit

--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -7,17 +7,17 @@ function Hero() {
         <h4>Support Portal</h4>
         <a href="">Track Tickets</a>
       </div>
-      <div className="row p-5 m-3">
-        <div className="col-6 p-3">
-          <h1 className="fs-3">
+      <div className="row p-3" id="supportContent">
+        <div className="col-6 ">
+          <h1 className="fs-4 mb-5">
             Search for an answer or browse help topics to create a ticket
           </h1>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <a className="mt-3" href="">Track account opening</a>
+          <a className="mt-3" href="">Track segment activation</a>
+          <a className="mt-3" href="">Intraday margins</a>
+          <a className="mt-3" href="">Kite user manual</a>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
