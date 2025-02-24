1541d30da52b2cf0e072fd4c43d4347f1779cdca
Disha Rathore <disharathore126@gmail.com>
2025-02-24 16:34:10 +0530

    New commit

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -10,22 +10,17 @@ function Team() {
         className="row p-3 text-muted fs-6"
         style={{ lineHeight: "1.8", fontSize: "1.2em" }}
       >
-        <div className="col p-3 text-center">
+        <div className="col p-3 text-center ">
           <img
-            src="media/image/nithinKamath.jpg"
-            style={{ borderRadius: "100%", width: "50%" }}
+            src="media/image/profile.jpg"
+            style={{ borderRadius: "100%", width: "30%" }}
           />
-          <h4 className="mt-3">Nithin Kamath</h4>
-          <h6 className="mt-3 ">Founder, CEO</h6>
+          <h4 className="mt-3">Disha Rathore</h4>
         </div>
         <div className="col p-3 ">
-          <p><br/>Nithin bootstrapped and founded FinXchange in 2010 to overcome the
-            hurdles he faced during his decade long stint as a trader. <br/>Today,
-            FinXchange has changed the landscape of the Indian broking industry.</p>
+          <p><br/>FinXchange is an innovative trading platform developed by Disha Rathore, a passionate frontend developer with a vision to create seamless financial solutions</p>
 
-          <p>He is a member of the SEBI Secondary Market Advisory Committee(SMAC) and the Market Data Advisory Committee <br />(MDAC).</p>
-
-          <p>Playing basketball is his zen.</p>
+          <p>FinXchange isn’t just a platform — it’s a commitment to innovation, making trading accessible, efficient, and powerful for users worldwide.</p>
 
           <p style={{textDecoration:"none"}}>Connect on <a href="#">Homepage</a> / <a href="#">TradingQnA</a> / <a href="#">Twitter</a> </p>
         </div>
